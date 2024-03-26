6994750d0f72f6cd727c5ad9f3ef550256da06c8
theshortsmonkey <j.horsley72@gmail.com>
2024-03-26 14:30:26 +0000

    image import working for test DB

--- db/mongooseTest.js
+++ db/mongooseTest.js
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose'
 import mongodb from 'mongodb'
 import fs from 'fs'
-import Gridfs from 'gridfs-stream'
 import { User } from './schema/User.js'
 import { Ride } from './schema/Ride.js'
 import { testUserData, testRideData, testFileNamesArr } from './test-data/test-seed-data.js'
-import { dirname } from 'path'
 
 const client = new mongodb.MongoClient('mongodb://localhost:27017')
 mongoose.connect('mongodb://localhost:27017/testJumpInDb')
@@ -14,14 +12,13 @@ const connection = mongoose.connection
 connection
   .dropDatabase()
   .then(() => {
-    const db = connection
+    const db = client.db('testJumpInDb')
     const bucket = new mongodb.GridFSBucket(db, {bucketName: 'images'})
     const __dirname = import.meta.dirname;
     testFileNamesArr.forEach((file) => {
         const streamWrite = bucket.openUploadStream(file)
         fs.createReadStream(`${__dirname}/test-data/${file}`).pipe(streamWrite)
     })
-    // return Promise.all([bucket, client.connect()])
     
 })
   .then(() => {
@@ -43,26 +40,4 @@ connection
     console.log('Test Data Seeded')
     return connection.close()
     
-// .then((promiseArr) => {
   })
-
-    // .on('close', (file) => {
-        // console.log(file)  
-        // imageUpload(promiseArr[0], testFileNamesArr)
-    // })
-// })
-// .then(() => {
-//     return client.close()
-// })
-    // .catch(console.log)
-    
-// function imageUpload(bucket, fileNamesArr) {
-//     const __dirname = import.meta.dirname;
-//     return fileNamesArr.forEach((file) => {
-//         const streamWrite = bucket.openUploadStream(file)
-//         fs.createReadStream(`${__dirname}/test-data/${file}`).pipe(streamWrite)
-//         streamWrite.on('close', (res) => {
-//             console.log(res, "<<<< res from on.close")
-//         })
-//     })
-// }
